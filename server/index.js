f588051a5f1b83484df7d5c4c9eac330a2f8f38d
Robert.SCHMID <robertschmid001@gmail.com>
2019-04-16 10:38:16 +0200

    x

--- server/index.js
+++ server/index.js
@@ -4,14 +4,14 @@ const cors = require('cors');
 const path = require("path");
 const app = express();
 
+app.get("/*", function (req, res) {
+  res.sendFile(path.join(__dirname + "/dist/index.html"));
+});
 // Middleware
 app.use(bodyParser.json());
 app.use(cors());
 
 app.use(express.static(__dirname + "/dist"));
-app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/index.html"));
-});
 
 const posts = require('./routes/api/trail');
 app.use('/api/trail', posts);

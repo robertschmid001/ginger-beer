598b10d8b64ed4eb3f64ff1cbaae9595b7b38474
Robert.SCHMID <robertschmid001@gmail.com>
2019-04-16 11:05:29 +0200

    x

--- server/index.js
+++ server/index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const path = require("path");
 const app = express();
 
-app.use(express.static(__dirname + "/dist"));
+app.use(express.static(__dirname + "/public"));
 app.get("/*", function (req, res) {
   res.sendFile(path.join(__dirname + "/dist/index.html"));
 });

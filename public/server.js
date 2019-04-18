6c23ee3d04ed31ea308e29ab6198e0b1616d84c0
Robert.SCHMID <robertschmid001@gmail.com>
2019-04-18 10:47:29 +0200

    x

--- public/server.js
+++ public/server.js
@@ -13,7 +13,7 @@ const app = express();
   
 app.use(express.static(__dirname + "/dist"));
 app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/index.html"));
+  res.sendFile(path.join(__dirname + "/index.html"));
 });
 // Middleware
 app.use(bodyParser.json());

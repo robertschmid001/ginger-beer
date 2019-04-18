87589dd519055beb3ff5c5bb1cf14f8e6152c530
Robert.SCHMID <robertschmid001@gmail.com>
2019-04-18 11:16:32 +0200

    x

--- server/index.js
+++ server/index.js
@@ -1,21 +1,21 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const cors = require('cors');
-const path = require("path");
-const app = express();
+// const express = require('express');
+// const bodyParser = require('body-parser');
+// const cors = require('cors');
+// const path = require("path");
+// const app = express();
 
-app.use(express.static(__dirname + "/dist"));
-app.get("/*", function (req, res) {
-  res.sendFile(path.join(__dirname + "/dist/index.html"));
-});
-// Middleware
-app.use(bodyParser.json());
-app.use(cors());
+// app.use(express.static(__dirname + "/dist"));
+// app.get("/*", function (req, res) {
+//   res.sendFile(path.join(__dirname + "/dist/index.html"));
+// });
+// // Middleware
+// app.use(bodyParser.json());
+// app.use(cors());
 
 
-const posts = require('./routes/api/trail');
-app.use('/api/trail', posts);
+// const posts = require('./routes/api/trail');
+// app.use('/api/trail', posts);
 
-const port = process.env.PORT || 5000;
+// const port = process.env.PORT || 5000;
 
-app.listen(port, () => console.log(`Server started on port ${port}`));
+// app.listen(port, () => console.log(`Server started on port ${port}`));

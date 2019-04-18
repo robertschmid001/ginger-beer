00552c65830e18b90ef377becbced40cba972134
Robert.SCHMID <robertschmid001@gmail.com>
2019-04-18 11:52:25 +0200

    x

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express();
   
   
 app.use(express.static(__dirname + "/dist"));
-app.get("/*", function (req, res) {
+app.get("*", function (req, res) {
   res.sendFile(path.join(__dirname + "/index.html"));
 });
 // Middleware
